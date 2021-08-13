42b7c353e68589bdb5ecd5ced271dda41fc1f425
Angus Robertson <angusprobertson@gmail.com>
2021-08-13 14:47:49 +0100

    DES encryption inital implementation

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,61 @@
 import sys
 import re
+import textwrap
+import math
+
+def leftShiftBin(inp):
+    out = ""
+    for i in range(len(inp)-1):
+        out = out + inp[i+1]
+    out = out + inp[0]
+    return out
+
+def desKeyComb(R, Kn):
+    E = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21,
+        20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
+    #SBOX
+    S_BOX = [
+        [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],[0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],[4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],[15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],],
+        [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],[3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],[0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],[13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],],
+        [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],[13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],[13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],[1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],],
+        [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],[13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],[10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],[3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],],
+        [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],[14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],[4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],[11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],],
+        [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],[10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],[9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],[4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],],
+        [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],[13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],[1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],[6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],],
+        [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],[1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],[7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],[2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],]]
+
+
+    ER = ""
+
+    for i in E:
+        ER = ER + R[i-1]
+
+    #print(R)
+    #print(ER)
+    #print(Kn)
+
+    KER = bin(int(ER, 2) ^ int(Kn, 2))[2:].zfill(48)
+
+    sBoxOut = ""
+
+    for i in range(8):
+        Bi = KER[i*6:(i+1)*6]
+        row = int(Bi[0] + Bi[5], 2)
+        col = int(Bi[1:5], 2)
+        sBoxOut = sBoxOut + bin(S_BOX[i][row][col])[2:].zfill(4)
+
+    #print(sBoxOut)
+
+    P = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
+        2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25]
+
+    output = ""
+
+    for i in P:
+        output = output + sBoxOut[i-1]
+
+
+    return output
 
 def encryptCaesar(plain, key):
     cipher = ""
@@ -13,8 +69,102 @@ def encryptCaesar(plain, key):
             newVal = newVal + 95
         cipher = cipher + chr( newVal )
     return cipher
-def encryptDES(plain):
-    return plain
+def encryptDES(plain, keyAsHexStr):
+    #STEP 1: Generate subkeys
+
+    keyAsDecInt = int(key, 16)
+    keyAsBinStr = bin(keyAsDecInt)
+    K = keyAsBinStr[2:].zfill(64)
+    K_Plus = ""
+    subK_Plus = []
+
+    PC_1 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,   10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+            63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,  14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
+
+    PC_2 = [14, 17, 11, 24, 1, 5, 3, 28,    15, 6, 21, 10, 23, 19, 12, 4,
+            26, 8, 16, 7, 27, 20, 13, 2,    41, 52, 31, 37, 47, 55, 30, 40,
+            51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+
+    for i in PC_1:
+        K_Plus = K_Plus + K[i-1]
+
+    #print((K))
+    #print((K_Plus))
+
+    C = [K_Plus[:28]]
+    D = [K_Plus[28:]]
+    #print((C))
+    #print((D))
+    #print(leftShiftBin(D))
+
+    shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+
+    for i in range(16):
+        C.append(leftShiftBin(C[i]) if shifts[i] == 1 else leftShiftBin(leftShiftBin(C[i])))
+        D.append(leftShiftBin(D[i]) if shifts[i] == 1 else leftShiftBin(leftShiftBin(D[i])))
+
+    #print(str(C))
+    #print(str(D))
+
+    CD = [C[i] + D[i] for i in range(17)]
+
+    #print(len(CD))
+
+    for i in range(16):
+        subK_Plus.append("")
+        for j in PC_2:
+            subK_Plus[i] = subK_Plus[i] + CD[i+1][j-1]
+
+    #print(subK_Plus)
+
+    #STEP 2: Encode data 64 bits at a time
+
+    IP_Mat = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,     62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,        61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
+
+    PI_1 = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,   38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,       34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
+
+    #NEEDS CORRECTING
+    plainAsHexStr = plain.encode("utf-8").hex()
+    plainAsDecInt = int(plainAsHexStr, 16)
+    plainAsBinStr = bin(plainAsDecInt)[2:]
+    length = 64 * math.ceil(len(plainAsBinStr)/64)
+    plainAsBinStr = plainAsBinStr.zfill(length)
+    #print(plainAsBinStr)
+
+    plainAsBinStrSegments = textwrap.wrap(plainAsBinStr, 64)
+
+    #print(plainAsBinStrSegments)
+
+    result = ""
+
+    for seg in plainAsBinStrSegments:
+        M = seg
+        IP = ""
+
+        for i in IP_Mat:
+            IP = IP + str(M[i-1])
+
+        #print(IP)
+
+        L = [IP[:32]]
+        R = [IP[32:]]
+
+        for i in range(16):
+            L.append(R[i])
+            R.append(bin(int(L[i], 2)^int(desKeyComb(R[i], subK_Plus[i]), 2))[2:].zfill(32))
+
+        RL = R[16] + L[16]
+
+        cipherAsBinStr = ""
+        for i in PI_1:
+            cipherAsBinStr = cipherAsBinStr + RL[i-1]
+
+        cipherAsDecInt = int(cipherAsBinStr, 2)
+        cipherAsHexStr = hex(cipherAsDecInt)[2:]
+        result = result + cipherAsHexStr
+    return result
 def encrypt3DES(plain):
     return plain
 def encryptAES(plain):
@@ -121,7 +271,14 @@ else:
 
     try:
         encryption = int(args[1])
-        key = int(args[2])
+        if(encryption == 1):
+            key = int(args[2])
+        elif(encryption == 2):
+            key = args[2]
+            if(not len(key) == 16):
+                raise ValueError()
+        else:
+            key = args[2]
         if not (encryption <= len(options) and encryption >= 1):
             raise ValueError()
     except ValueError:
@@ -131,7 +288,8 @@ else:
         for n in options.keys():
             print(str(n)+ " : " + options[n], end=" | " if n != len(options) else "")
         print("]")
-        print("\nENCRYPTION and KEY should be integer values\n")
+        print("\nENCRYPTION should be integer value\n")
+        print("\KEY should be either integer value or hexadecimal for longer keys\n")
         print("\nEnsure you use single quotes ('') for PLAINTEXT to avoid terminal misreading\n")
         sys.exit()
     args.pop(0)
@@ -145,7 +303,7 @@ output = ""
 if(encryption == 1):
     output = encryptCaesar(plaintext, key)
 elif(encryption == 2):
-    output = encryptDES(plaintext)
+    output = encryptDES(plaintext, key)
 elif(encryption == 3):
     output = encrypt3DES(plaintext)
 elif(encryption == 4):
